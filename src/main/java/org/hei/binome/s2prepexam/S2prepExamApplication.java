b35280dae6e17bf27f28d2f2175999bbb215af96
K. <148479852+Kiady06@users.noreply.github.com>
2026-08-18 13:11:30 +0300

    initial commmit

---
+++ src/main/java/org/hei/binome/s2prepexam/S2prepExamApplication.java
@@ -0,0 +1,13 @@
+package org.hei.binome.s2prepexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2prepExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2prepExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hei/binome/s2prepexam/S2prepExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.binome.s2prepexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2prepExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
